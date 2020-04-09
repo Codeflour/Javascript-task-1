488e3dfc8e75517a94f9246d150ec9241e0010f8
Codeflour <cynthianuchepro@gmail.com>
2020-04-09 11:36:57 +0100

    first commit

---
+++ javascript task 1.js
@@ -0,0 +1,15 @@
+let myName = "Cynthia Uche"
+let myCourses = ["Html", "Css", "Javascript", "Python"]
+console.log("My name is " + myName + " and my courses are" + " " + myCourses)
+if (myCourses.length % 2 == 0) {
+  for (var i = 0; i <= 200; i++) {
+
+    console.log(i++)
+
+  }
+}
+else {
+  for (var i = 1; i <= 200; i++) {
+    console.log(i++)
+  }
+}
